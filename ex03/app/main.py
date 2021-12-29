ede45850ca4d4f9e4f180a76f9c29939ad6188c4
Jesse Rau <Jesse.Rau@outlook.com>
2021-12-29 16:35:47 -0600

    update ex03 code again

--- ex03/app/main.py
+++ ex03/app/main.py
@@ -1,1 +1,19 @@
-#
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+class Package(BaseModel):
+   name: str
+   number: str
+   description: Optional[str] = None
+
+app = FastAPI()
+
+
+@app.get('/')
+async def hello_world():
+   return {'Hello' : 'World'}
+
+@app.post("/package/{priority}")
+async def make_package(priority: int, package: Package, value: bool):
+   return {"priority": priority, **package.dict(), "value": value}
